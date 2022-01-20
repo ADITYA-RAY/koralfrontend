04f557499918001dfc76f6d888e9826070eaa3ac
ADITYA-RAY <74045024+ADITYA-RAY@users.noreply.github.com>
2022-01-20 13:22:26 +0530

    form integration with backend

--- src/compnents/Contact.jsx
+++ src/compnents/Contact.jsx
@@ -1,12 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { VscTriangleDown } from "react-icons/vsc";
+import axios from "axios";
 
 export const Contact = () => {
-  const showOptions = () => {
-    document.getElementById("dropdown").style.display = "block";
+  const [selected, setSelected] = useState("web design");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    document.getElementById("selected").innerHTML = selected;
+  });
+
+  const showOptions = (value) => {
+    setSelected(value);
+    document.getElementById("selected").innerHTML = selected;
+  };
+  const handleSubmit = () => {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+      const payload = {
+        name: name,
+        email: email,
+        subject: subject,
+        service: selected,
+        message: message,
+      };
+      axios
+        .post("https://koralbackend.herokuapp.com/api/quotation", payload)
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          alert(error);
+          console.log(error);
+        });
+
+      console.log(payload);
+    } else {
+      alert("enter a valid email");
+    }
   };
   return (
     <div>
@@ -16,46 +52,109 @@ export const Contact = () => {
       <div className="contact_card">
         <Container>
           <br />
+
           <Row>
             <Col xs={12} md={6}>
               <div className="label">Your Name(Required)</div>
-              <input type="text" />
+              <input
+                type="text"
+                id="f_name"
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+              />
             </Col>
             <Col xs={12} md={6}>
               <div className="label">Your Email(Required)</div>
-              <input type="text" />
+              <input
+                type="email"
+                id="f_email"
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
             </Col>
             <Col xs={12} md={6}>
               <div className="label">Subject</div>
-              <input type="text" />
+              <input
+                type="text"
+                onChange={(e) => {
+                  setSubject(e.target.value);
+                }}
+              />
             </Col>
             <Col xs={12} md={6}>
               <div className="label">Service Desired</div>
-              <div
-                className="select"
-                onClick={() => {
-                  showOptions();
-                }}
-                id="select"
-              >
+              <div className="select" id="select">
+                <div id="selected"></div>
                 <VscTriangleDown />
               </div>
               <div className="dropdown" id="dropdown">
-                <div className="droplink">Web Design</div>
-                <div className="droplink">Branding</div>
-                <div className="droplink">Web APS</div>
-                <div className="droplink">Graphic</div>
-                <div className="droplink">Support</div>
+                <div
+                  className="droplink"
+                  onClick={() => {
+                    showOptions("web design");
+                  }}
+                >
+                  Web Design
+                </div>
+                <div
+                  className="droplink"
+                  onClick={() => {
+                    showOptions("branding");
+                  }}
+                >
+                  Branding
+                </div>
+                <div
+                  className="droplink"
+                  onClick={() => {
+                    showOptions("web aps");
+                  }}
+                >
+                  Web APS
+                </div>
+                <div
+                  className="droplink"
+                  onClick={() => {
+                    showOptions("graphic");
+                  }}
+                >
+                  Graphic
+                </div>
+                <div
+                  className="droplink"
+                  onClick={() => {
+                    showOptions("support");
+                  }}
+                >
+                  Support
+                </div>
               </div>
             </Col>
             <Col xs={12} md={12}>
-              <div className="label">Subject</div>
-              <textarea name="message"></textarea>
+              <div className="label">Message</div>
+              <textarea
+                name="message"
+                id="message"
+                onChange={(e) => {
+                  setMessage(e.target.value);
+                }}
+              ></textarea>
             </Col>
           </Row>
+
           <br />
           <center>
-            <button className="c_btn">SEND QUOTE</button>
+            <button
+              type="submit"
+              className="c_btn"
+              onClick={() => {
+                handleSubmit();
+              }}
+            >
+              GET QUOTE
+            </button>
           </center>
           <br />
         </Container>
